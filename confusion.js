e52fe11665d6d45911e717a914a01735095cd62a
Shivanand Satao <sataoshivanand@gmail.com>
2026-02-07 17:12:34 +0530

    done with the confusing code mostly asked in the interview

---
+++ confusion.js
@@ -0,0 +1,6 @@
+console.log("1"+2)
+console.log(1+"2")
+console.log("1"+2+2)
+console.log(1+2+"2")
+//it will depend on the type of the first argumetn which type it is it will return teh exact 
+//and will convert the rest into that 
