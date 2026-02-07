a238d005ca04f0791ed69c611d11971bde59fb11
Shivanand Satao <sataoshivanand@gmail.com>
2026-02-07 16:59:48 +0530

    a

--- datatypes_ecma.js
+++ datatypes_ecma.js
@@ -1,2 +1,70 @@
+//direclty type cast the number in one another 
+
+let b =Number ("123");
+
+console.log(typeof b);
+console.log(b)
+
+let c=Number ("1234abc")
+
+console.log(typeof c);
+console.log(c)
+
+// type of nan is number 
+//true false normal if any thign is empty then false else try e
+ 
+
+
+
+
+
+
+
+
+
+
+//here the number is converted and the nan means is not a number 
 //type of null is undefined 
 // we use the type of attribute to find the type of each attribute
+//type of undefined  is undefined
+
+// 🔤 JavaScript Data Types (Official)
+// ✅ Primitive Data Types
+
+// Number
+
+// String
+
+// Boolean
+
+// Undefined
+
+// Null
+
+// Symbol
+
+// BigInt
+
+// 👉 These store single values
+
+// ✅ Non-Primitive (Reference) Data Types
+
+// Object
+
+// Array
+
+// Function
+
+// Date
+
+// RegExp
+
+// Map / Set
+
+// ⚠️ Internally:
+
+// Arrays, functions, dates — all are objects
+//
+
+let a =10;
+console.log(typeof a)
